3f91d5d3a5e3294944f92f893dc632a48bc51498
Dzuhaib <zuhaibahmed3213951@gmail.com>
2026-07-25 18:37:45 +0500

    Update Facebook Pixel ID to 1422951686240740

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1385365646380826');
+            fbq('init', '1422951686240740');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -39,7 +39,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1385365646380826&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1422951686240740&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>

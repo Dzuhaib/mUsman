95d1dfe6bd6767fc63dba7541bc5936998f9d1dd
Dzuhaib <zuhaibahmed3213951@gmail.com>
2026-08-03 15:31:18 +0500

    Fix Meta Pixel detection by inlining pixel script before interactive

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" dir="ltr">
       <head>
-        <Script id="meta-pixel" strategy="afterInteractive">
+        <Script id="meta-pixel" strategy="beforeInteractive">
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -34,20 +34,6 @@ export default function RootLayout({
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '1661442715630637');
             fbq('init', '1331161572496594');
-            fbq('init', '1042429051532542');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <Script id="meta-pixel-canonical" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '1042429051532542');
             fbq('track', 'PageView');
           `}
